6224789eb03dc67ee2ff85762685500e39705c8a
Sc0tticus <ssinger303@gmail.com>
2022-03-01 12:37:10 -0700

    CH4. Server not running due to linter errrors

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('hello');
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors(options: {
+	origin: ["http://localhost:3000"]
+}));
+
+
+app.get('/', (req: Request, res: Response) => {
+	res.send(body: 'Hello World');
+});
+
+app.listen(port: 8000, callback: () => {
+	console.log('listening to port 8000')
+});
